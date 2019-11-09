f79ca7c80926cb7eb998a02bbf41f084b9dfd72b
Josef <josutb-7@student.ltu.se>
2019-11-09 22:32:14 +0100

    Fixed some databas handling for products

--- code/SQL_handler.py
+++ code/SQL_handler.py
@@ -9,13 +9,16 @@ connection = pymysql.connect(host='localhost',
                              charset='utf8mb4',
                              cursorclass=pymysql.cursors.DictCursor)
 
+
 class NotInDatabase(Exception):
    # Raised when an item doesn't exist in the database
    pass
 
+
 class IncorrectProductDeclaration(Exception):
    # Raised when an attempt at creating a product object fails
    pass
+
 
 class DuplicationError(Exception):
    # Raised when an attempt at inserting a duplicate object into the database
@@ -33,17 +36,22 @@ class Product():
 		self.id = values.get("id")
 		self.name = values.get("name")	
 
+
 # Returns a product item from the database as a dict
 def getProductFromDatabase(id):
-    with connection.cursor() as cursor:
-        cursor.execute("SELECT * FROM product WHERE id=%s", (id, ))
-        try:
-        	return Product((dict)(cursor.fetchone())) # Why does this work? This shouldn't be a feature!
-        except:
-        	raise NotInDatabase
+	global connection
+
+	with connection.cursor() as cursor:
+		cursor.execute("SELECT * FROM product WHERE id=%s", (id, ))
+	try:
+		return Product((dict)(cursor.fetchone())) # Why does this work? This shouldn't be a feature!
+	except:
+		raise NotInDatabase
 
 
 def insertProductIntoDatabase(product: Product) -> None:
+	global connection
+
 	if not isinstance(product, Product):
 		raise TypeError
 
@@ -55,7 +63,30 @@ def insertProductIntoDatabase(product: Product) -> None:
 			raise DuplicationError
 
 
+def updateProductIntoDatabase(product: Product) -> None:
+	global connection
 
-# product = Product({'id': '000003', 'name': 'Ompa Drive'})
+	if not isinstance(product, Product):
+		raise TypeError
+
+	with connection.cursor() as cursor:		
+		cursor.execute("UPDATE `product` SET `name`=%s WHERE `id`=%s", (product.name, product.id));
+
+
+# Returns a list of ids from the database
+def getAllProductId():
+	global connection
+
+	with connection.cursor() as cursor:		
+		cursor.execute("SELECT id FROM product");
+		result = cursor.fetchall()
+
+		return [instance.get("id") for instance in result]
+
+
+# product = Product({'id': '000003', 'name': 'Empa Drive'})
 # insertProductIntoDatabase(product)
-print(getProductFromDatabase('000003').name)
+# updateProductIntoDatabase(product)
+# print(getProductFromDatabase('000003').name)
+
+# print(getAllProductId())
